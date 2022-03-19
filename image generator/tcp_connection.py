1973fe29d1625b772fc3682c80ae78e3b746093e
Kaisiq <dahudohuto@abv.bg>
2022-03-19 15:56:39 +0200

    add CLIP recognition into tcp_connection and create a test function

--- image generator/tcp_connection.py
+++ image generator/tcp_connection.py
@@ -1,9 +1,15 @@
 import socket
+import os
+
+import PIL.Image
+import clip
+import torch
+from torchvision.datasets import CIFAR100
 
 while True:
 
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server.bind('localhost', 6666)  #ip = 127.0.0.1 , port = 6666
+    server.bind(('localhost', 6666))  #ip = 127.0.0.1 , port = 6666
     server.listen()
 
     client_socket, client_address = server.accept()
@@ -19,6 +25,39 @@ while True:
 
 
     # снимката с име "image_to_be_given_to_CLIP" се подава на първия код, който ползва CLIP за да разпознае на какво прилича снимката
+
+    # Load the model
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    model, preprocess = clip.load('ViT-B/32', device)
+
+    # Download the dataset
+    cifar100 = CIFAR100(root=os.path.expanduser("~/.cache"), download=True, train=False)
+
+    # Prepare the inputs
+    image = PIL.Image.open('image_to_be_given_to_CLIP')
+    image_input = preprocess(image).unsqueeze(0).to(device)
+    text_inputs = torch.cat([clip.tokenize(f"a photo of a {c}") for c in cifar100.classes]).to(device)
+
+    # Calculate features
+    with torch.no_grad():
+        image_features = model.encode_image(image_input)
+        text_features = model.encode_text(text_inputs)
+
+    # Pick the top 5 most similar labels for the image
+    image_features /= image_features.norm(dim=-1, keepdim=True)
+    text_features /= text_features.norm(dim=-1, keepdim=True)
+    similarity = (100.0 * image_features @ text_features.T).softmax(dim=-1)
+    values, indices = similarity[0].topk(5)
+
+    # Print the result
+    print("\nTop predictions:\n")
+    for value, index in zip(values, indices):
+        print(f"{cifar100.classes[index]:>16s}: {100 * value.item():.2f}%")
+
+
+
+
+
     # първите 5 неща, на които снимката прилича, биват запаметени в стрингове 1 - 5
     # всеки от тези 5 стринга бива подаден на VQGAN + CLIP, които генерират снимки 1 - 5
 
---
+++ image generator/test_for_tcp_connection.py
@@ -0,0 +1,14 @@
+import socket
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(('localhost', 6666))  #ip = 127.0.0.1 , port = 6666
+
+file = open('luba.jpg', 'rb')
+image_data = file.read(4096)
+
+while image_data:
+    client.send(image_data)
+    image_data = file.read(4096)
+
+file.close()
+client.close()
