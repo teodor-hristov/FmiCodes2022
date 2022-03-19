9173d420ac2e71840709e786fb6978263460f27e
PavlaManova <48806251+PavlaManova@users.noreply.github.com>
2022-03-19 21:27:27 +0200

    add canvas and buttons for changing colors

--- mobile folder/app/src/main/java/com/example/drawingapp/MainActivity.java
+++ mobile folder/app/src/main/java/com/example/drawingapp/MainActivity.java
@@ -1,13 +1,10 @@
 package com.example.drawingapp;
 
 import android.os.Bundle;
-
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
-
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-
 import android.view.View;
 
 import android.view.Menu;
@@ -22,7 +19,7 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
+       FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -31,8 +28,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
-
-    @Override
+@Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
